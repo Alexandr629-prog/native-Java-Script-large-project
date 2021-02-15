24951a33ecbc978d8cfcca4567189c408f661c16
Alex <aaskaldovic@gmail.com>
2021-02-15 21:29:46 +0300

    fix bag

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist ='../../../../MAMP/htdocs/picture_mamp/';
 //const dist = "./dist/";
+const dist = '../../../../MAMP/htdocs/picture_mamp/';
+
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,19 +1,55 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import {getResourse} from '../services/requests';
 
-    cards.forEach(card => {
-        card.classList.add('animated', 'fadeInUp');
+const showMoreStyles = (trigger, wrapper) => {
+        const btn = document.querySelector(trigger);
+
+    // cards.forEach(card => {
+    //     card.classList.add('animated', 'fadeInUp');
+    // });
+
+    // btn.addEventListener('click', () => {
+    //     cards.forEach(card => {
+    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    //     });
+    //     // btn.style.display = 'none';
+    //     btn.remove();
+    // });
+
+    btn.addEventListener('click', function(){
+        getResourse('assets/db.json')
+        .then(res => {
+            this.remove();
+            createCards(res.styles);
+        })
+        .catch(error =>
+            {faildResponsse();
+             console.log(error);
+            });
+        
     });
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+    function createCards(respoonse){
+        respoonse.forEach(item=>{
+            let card = document.createElement('div');
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML =`
+            <div class=styles-block>
+             <img src=${item.src} alt="style">
+           <h4>${item.title}</h4>
+            <a href${item.link}Подробнее</a>
+            </div>`;
+            document.querySelector(wrapper).appendChild(card);
         });
-        // btn.style.display = 'none';
-        btn.remove();
-    });
+    }
+    function faildResponsse(){
+        let fail = document.createElement('div');
+        fail.classList.add('animated', 'fadeInUp', 'status');
+        fail.textContent = 'что-то пошло не так...';
+        document.querySelector(wrapper).appendChild(fail);
+    }
 };
 
 export default showMoreStyles;
